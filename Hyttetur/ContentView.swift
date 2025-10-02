822bfc3e3573f3c7e11163ac92357e6db48e9705
Kristin Ottesen-Næss <kriote@hotmail.com>
2025-10-02 18:17:34 +0200

    Initial commit

---
+++ Hyttetur/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hyttetur
+//
+//  Created by Kristin Ottesen-Næss on 02/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hyttetur/HytteturApp.swift
@@ -0,0 +1,17 @@
+//
+//  HytteturApp.swift
+//  Hyttetur
+//
+//  Created by Kristin Ottesen-Næss on 02/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct HytteturApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
